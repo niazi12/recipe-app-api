c8a84a6be4c74b26f6bf3980e5acbafe24231fcd
Niazi Mahrab <mahrab570@gmail.com>
2023-10-27 17:53:48 +0600

    management added

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Django command to wait for db
+"""
+from typing import Any
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
+    
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,22 @@
+"""
+django management commands
+"""
+
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test command"""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for db when db is available"""     
+        patched_check.return_value = True
+        call_command('wait_for_db')
+        patched_check.assert_called_once_with(database=['default'])
