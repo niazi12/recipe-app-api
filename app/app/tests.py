eadbd089968059c486fcc33db283048909ba2127
niazi12 <mahrab570@gmail.com>
2023-10-28 23:22:25 +0600

    wait for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,9 @@
 from django.test import SimpleTestCase
-
 from app import caltest
+
+
 class CalscTest(SimpleTestCase):
+
     def test_add(self):
-        res = caltest.add(2,3)
-        
+        res = caltest.add(2, 3)
         self.assertEqual(res, 5)
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -16,7 +16,18 @@ class CommandTests(SimpleTestCase):
     """Test command"""
 
     def test_wait_for_db_ready(self, patched_check):
-        """Test waiting for db when db is available"""     
+        """Test waiting for db when db is available"""
+
         patched_check.return_value = True
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for db"""
+
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+        call_command('wait_for_db')
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
